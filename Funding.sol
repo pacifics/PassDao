a828b6d4175ff4dfbced8ee9eafcc488a0bec2a0
Pass DAO <frederic.vedrunes@gmail.com>
2016-10-12 00:34:24 +0300

    Update Funding.sol

--- Funding.sol
+++ Funding.sol
@@ -17,7 +17,7 @@ along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /*
- * Standard smart contract used for the funding of the Dao.
+ * Smart contract used for the funding of the Dao.
 */
 
 import "AccountManager.sol";
