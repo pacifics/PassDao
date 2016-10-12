1662540e2c1d3b82a868703c0bd0fd2255f42f8e
Pass DAO <frederic.vedrunes@gmail.com>
2016-10-12 11:18:02 +0300

    Update Dao.sol

--- Dao.sol
+++ Dao.sol
@@ -577,7 +577,7 @@ contract DAO is DAOInterface
 
         if (b.dateOfExecution == 0 || c.weightToRecieve[_Tokenholder]==0 || c.totalAmountForTokenReward == 0) {throw; }
         
-        uint _weight = c.weightToRecieve[_Tokenholder]
+        uint _weight = c.weightToRecieve[_Tokenholder];
         c.weightToRecieve[_Tokenholder] = 0;
         uint _amount = (c.totalAmountForTokenReward*_weight)/c.totalWeight;
 
