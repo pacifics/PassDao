5e75f950fd4f544e9bc9eab445a4dc004a162ab7
Pass DAO <frederic.vedrunes@gmail.com>
2016-10-21 23:03:52 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,104 +1,84 @@
 pragma solidity ^0.4.2;
 
 /*
-Basic, standardized Token contract with no "premine". Defines the functions to
+Token contract with no "premine". Defines the functions to
 check token balances, send tokens, send tokens on behalf of a 3rd party and the
 corresponding approval process.
 */
 
-contract TokenInterface {
+contract Token {
+
+    /* Array with all balances */
     mapping (address => uint256) balances;
+    
+    /* Array with all allowances */
     mapping (address => mapping (address => uint256)) allowed;
 
-    /// Total amount of tokens
+    /* Total amount of tokens */
     uint256 public totalSupply;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) returns (bool success);
+    /* Amount of decimals for token display purposes */
+    uint8 public decimals;
+ 
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    
+    /* Send coins */
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value
+            && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+           return false;
+        }
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) returns (bool success) {
+        if (balances[_from] >= _value
+            && allowed[_from][msg.sender] >= _value
+            && balances[_to] + _value > balances[_to]) {
+
+            balances[_from] -= _value;
+            balances[_to] += _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on
     /// its behalf
     /// @param _spender The address of the account able to transfer the tokens
     /// @param _amount The amount of tokens to be approved for transfer
     /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _amount) returns (bool success);
-
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens of _owner that _spender is allowed
-    /// to spend
-    function allowance(
-        address _owner,
-        address _spender
-    ) constant returns (uint256 remaining);
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
-}
-
-contract Token is TokenInterface {
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function transfer(address _to, uint256 _amount) returns (bool success) {
-
-        if (balances[msg.sender] >= _amount
-            && balances[_to] + _amount > balances[_to]) {
-            balances[msg.sender] -= _amount;
-            balances[_to] += _amount;
-            Transfer(msg.sender, _to, _amount);
-            return true;
-        } else {
-           return false;
-        }
-
-    }
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) returns (bool success) {
-
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && balances[_to] + _amount > balances[_to]) {
-
-            balances[_from] -= _amount;
-            balances[_to] += _amount;
-            allowed[_from][msg.sender] -= _amount;
-            Transfer(_from, _to, _amount);
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
     function approve(address _spender, uint256 _amount) returns (bool success) {
         allowed[msg.sender][_spender] = _amount;
         Approval(msg.sender, _spender, _amount);
         return true;
     }
 
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens of _owner that _spender is allowed
+    /// to spend
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
 
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+     function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+     }
+     
 }
