5549b8f506edb085cebe86d7d35e4cf010df9a8d
Pass DAO <frederic.vedrunes@gmail.com>
2016-11-02 15:27:28 +0300

    Update PassDAOCreator.sol

--- PassDAOCreator.sol
+++ PassDAOCreator.sol
@@ -252,8 +252,8 @@ contract PassDAO {
             || _recipient == address(daoAccountManager)
             || _amount == 0
             || (lastRecipientProposalId[_recipient] != 0 
-                && ((contractorAccountManager[_recipient].TotalSupply() != 0 && c.totalAmountForTokenReward != 0)
-                    || (msg.sender != c.recipient && !contractorAccountManager[c.recipient].IsCreator(msg.sender))
+                && ((contractorAccountManager[_recipient].TotalSupply() != 0 && _totalAmountForTokenReward != 0)
+                    || (msg.sender != _recipient && !contractorAccountManager[_recipient].IsCreator(msg.sender))
                     || c.initialSupply != 0))) throw;
 
         uint _ContractorProposalID = ContractorProposals.length++;
