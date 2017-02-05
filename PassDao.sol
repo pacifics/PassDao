ce145bb89b206009b0eb183eca032339d2821b82
Pass DAO <frederic.vedrunes@gmail.com>
2017-02-06 00:57:49 +0300

    Update PassDao.sol

--- PassDao.sol
+++ PassDao.sol
@@ -33,10 +33,6 @@ contract PassDao {
         // Address of the token manager smart contract
         address tokenManager;
         // Address of the project creator smart contract
-        address projectCreator;
-        // Address of the contractor creator smart contract
-        address contractorCreator;
-        // The unix effective date of the contract
         uint startDate;
     }
     // The revisions of the Dao until today
@@ -60,8 +56,7 @@ contract PassDao {
     
 // Events
 
-    event Upgrade(uint indexed RevisionID, address CommitteeRoom, address ShareManager, address TokenManager, 
-        address ProjectCreator, address ContractorCreator);
+    event Upgrade(uint indexed RevisionID, address CommitteeRoom, address ShareManager, address TokenManager);
     event NewProject(address Project);
 
 // Constant functions  
@@ -86,16 +81,6 @@ contract PassDao {
         return revisions[0].tokenManager;
     }
 
-    /// @return The effective project Creator
-    function ActualProjectCreator() constant returns (address) {
-        return revisions[0].projectCreator;
-    }
-
-    /// @return The effective contractor Creator
-    function ActualContractorCreator() constant returns (address) {
-        return revisions[0].contractorCreator;
-    }
-
 // modifiers
 
     modifier onlyPassCommitteeRoom {if (msg.sender != revisions[0].committeeRoom  
@@ -114,15 +99,11 @@ contract PassDao {
     /// @param _newCommitteeRoom The address of the new committee room
     /// @param _newShareManager The address of the new share manager
     /// @param _newTokenManager The address of the new token manager
-    /// @param _newProjectCreator The address of the new project creator smart contract
-    /// @param _newContractorCreator The address of the new contractor creator smart contract
     /// @return The index of the revision
     function upgrade(
         address _newCommitteeRoom, 
         address _newShareManager, 
-        address _newTokenManager,
-        address _newProjectCreator,
-        address _newContractorCreator) onlyPassCommitteeRoom returns (uint) {
+        address _newTokenManager) onlyPassCommitteeRoom returns (uint) {
         
         uint _revisionID = revisions.length++;
         revision r = revisions[_revisionID];
@@ -130,14 +111,12 @@ contract PassDao {
         if (_newCommitteeRoom != 0) r.committeeRoom = _newCommitteeRoom; else r.committeeRoom = revisions[0].committeeRoom;
         if (_newShareManager != 0) r.shareManager = _newShareManager; else r.shareManager = revisions[0].shareManager;
         if (_newTokenManager != 0) r.tokenManager = _newTokenManager; else r.tokenManager = revisions[0].tokenManager;
-        if (_newProjectCreator != 0) r.projectCreator = _newProjectCreator; else r.projectCreator = revisions[0].projectCreator;
-        if (_newContractorCreator != 0) r.contractorCreator = _newContractorCreator; else r.contractorCreator = revisions[0].contractorCreator;
 
         r.startDate = now;
         
         revisions[0] = r;
         
-        Upgrade(_revisionID, _newCommitteeRoom, _newShareManager, _newTokenManager, _newProjectCreator, _newContractorCreator);
+        Upgrade(_revisionID, _newCommitteeRoom, _newShareManager, _newTokenManager);
             
         return _revisionID;
     }
